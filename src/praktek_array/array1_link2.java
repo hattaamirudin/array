54eca33d29d267701f2ab443a30b3968d1a1a2ef
Fujitsu <Fujitsu@WINCTRL-RPQUVVL>
2020-07-16 14:29:09 +0700

    ini project saya

---
+++ src/praktek_array/array1_link2.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package praktek_array;
+
+/**
+ *
+ * @author Fujitsu
+ */
+public class array1_link2 {
+    public static void main(String[] args) {
+       
+        String[] teman = {"Aku","Kamu","Dia","Kita"};
+        
+        for (int i = 0; i < teman.length; i++){
+            System.out.println("Indeks ke-"+i+": "+ teman [i]);
+        }
+    }
+
+}
